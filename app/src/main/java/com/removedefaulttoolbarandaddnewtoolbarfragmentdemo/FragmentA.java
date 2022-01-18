011b0c5ae75bafe0f59c2d1da3e7feeac01a9759
Coder-Matheo <ali.dinarvand1370@gmail.com>
2022-01-18 13:45:56 +0100

    Remove Default Toolbar and Add New ToolBar Fragment Demo

---
+++ app/src/main/java/com/removedefaulttoolbarandaddnewtoolbarfragmentdemo/FragmentA.java
@@ -0,0 +1,36 @@
+package com.removedefaulttoolbarandaddnewtoolbarfragmentdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class FragmentA extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = getLayoutInflater().inflate(R.layout.a_fragment, container, false);
+
+        return view;
+    }
+
+    //Hide and Show Toolbar in this Fragment, for add in xml file
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
+
+
+}
---
+++ app/src/main/java/com/removedefaulttoolbarandaddnewtoolbarfragmentdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.removedefaulttoolbarandaddnewtoolbarfragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FragmentA fragmentA = new FragmentA();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragmentA).commit();
+    }
+}
